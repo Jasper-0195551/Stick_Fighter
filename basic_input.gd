ad8a877f49ed7900fdb452b826ed5c5739d94a57
Jasper <0195551@schoolsnet.act.edu.au>
2022-09-02 17:35:07 +1000

    Initial commit

---
+++ basic_input.gd
@@ -0,0 +1,43 @@
+extends KinematicBody2D
+
+onready var animatedSprite = $AnimatedSprite
+
+var isinCombo = false
+
+var timeTillNextInput = 0.5
+var time = 0
+# var b = "text"
+var currentAttack = 0
+var previousAttack = 0
+
+func _ready():
+	time = timeTillNextInput
+
+
+func _process(delta):
+	if(Input.is_action_just_pressed("ui_punch")):
+		if(currentAttack == 0):
+			animatedSprite.play("Straight Punch")
+		elif(currentAttack == 1):
+			animatedSprite.play("Upper Cut")
+		elif(currentAttack == 2):
+			animatedSprite.play("Low Kick")
+
+		isinCombo = true
+		currentAttack += 1
+
+	if(isinCombo):
+		time -= delta
+		
+		
+		if(time < 0):
+			time = timeTillNextInput
+			isinCombo = false
+			currentAttack = 0
+			animatedSprite.play("Idle")
+			
+			
+			
+func _Reset_Attack_Timer():
+	if(currentAttack < 4):
+		time = timeTillNextInput
